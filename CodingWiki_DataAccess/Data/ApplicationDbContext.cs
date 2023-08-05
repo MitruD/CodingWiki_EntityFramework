bd7169e22e786c0c4ed030c93aa7016c2b5027fb
Cronos <dimamitru@gmail.com>
2023-08-05 15:26:17 +0300

    Migrations: Create ApplicationDbContext and Book Model

---
+++ CodingWiki_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CodingWiki_Model.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingWiki_DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //Name of the property will be the table name
+        public DbSet<Book> Books { get; set; }
+    }
+}
