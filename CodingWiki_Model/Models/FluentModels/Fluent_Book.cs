a5a0aa60fef559bbacc7335ef5653545f191c5d6
Cronos <dimamitru@gmail.com>
2023-08-07 13:43:26 +0300

    "Many to Many" Relation

--- CodingWiki_Model/Models/FluentModels/Fluent_Book.cs
+++ CodingWiki_Model/Models/FluentModels/Fluent_Book.cs
@@ -19,6 +19,8 @@ namespace CodingWiki_Model.Models
         public Fluent_BookDetail BookDetail { get; set; }
         public int Publisher_Id { get; set; }
         public Fluent_Publisher Publisher { get; set; }
+        //public List<Fluent_Author> Authors { get; set; }
+
 
         //public List<Fluent_BookAuthorMap> BookAuthorMap { get; set; }
     }
