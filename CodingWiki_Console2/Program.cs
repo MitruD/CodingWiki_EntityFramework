928ee4b0249a4918a153974006c9bfb79288604c
Cronos <dimamitru@gmail.com>
2023-08-08 11:45:25 +0300

    Seed Category and Logging in EF Core

--- CodingWiki_Console2/Program.cs
+++ CodingWiki_Console2/Program.cs
@@ -23,15 +23,15 @@ Console.WriteLine("Hello, World!");
 //AddBook();
 //GetBook();
 //UpdateBook();
-DeleteBook();
+//DeleteBook();
 
-async void DeleteBook()
-{
-    using var context = new ApplicationDbContext();
-    var book = await context.Books.FindAsync(5);
-    context.Books.Remove(book);
-    await context.SaveChangesAsync();
-}
+//async void DeleteBook()
+//{
+//    using var context = new ApplicationDbContext();
+//    var book = await context.Books.FindAsync(5);
+//    context.Books.Remove(book);
+//    await context.SaveChangesAsync();
+//}
 
 //void DeleteBook()
 //{
@@ -55,83 +55,83 @@ async void DeleteBook()
 
 //    }
 //}
-void UpdateBook()
-{
-    try
-    {
-        using var context = new ApplicationDbContext();
-        var books = context.Books.Where(u=>u.Publisher_Id==1);
-        foreach (var book in books)
-        {
-            book.Price = 55.55m;
-        }
-        context.SaveChanges();
-    }
-    catch (Exception e)
-    {
-
-    }
-}
-
-void GetBook()
-{
-    using var context = new ApplicationDbContext();
-    //var books = context.Books.Where(u=>u.ISBN.Contains("12"));
-    //var books = context.Books.Where(u=>EF.Functions.Like(u.ISBN, "12%")).Max(u => u.Price);
-    //var books = context.Books.Where(u=>EF.Functions.Like(u.ISBN, "12%")).Count();
-    //var books = context.Books.Where(u => EF.Functions.Like(u.ISBN, "12%"));
-    //var books = context.Books.Where(u => u.Price > 10).OrderBy(u => u.Title).ThenByDescending(u => u.ISBN);
-    
-    //Pagination Methods
-    var books = context.Books.Skip(0).Take(2);
-   
-    //Console.WriteLine(book.Title + " - " + book.ISBN);
-    foreach (var book in books)
-    {
-        Console.WriteLine(book.Title + " - " + book.ISBN);
-    }
-}
-//void GetBook()
-//{
-//    using var context = new ApplicationDbContext();
-//    Book book = context.Books.First();
-//    Console.WriteLine(book.Title + " - " + book.ISBN);
-//}
-
-//void GetBook()
+//void UpdateBook()
 //{
 //    try
 //    {
-//    using var context = new ApplicationDbContext();
-//    var book = context.Fluent_Books.FirstOrDefault();
-//    Console.WriteLine(book.Title + " - " + book.ISBN);
-//    }catch(Exception e)
+//        using var context = new ApplicationDbContext();
+//        var books = context.Books.Where(u => u.Publisher_Id == 1);
+//        foreach (var book in books)
+//        {
+//            book.Price = 55.55m;
+//        }
+//        context.SaveChanges();
+//    }
+//    catch (Exception e)
 //    {
 
 //    }
 //}
 
-void GetAllBooks()
-{
-    using var context = new ApplicationDbContext();
-    var books = context.Books.ToList();
-    foreach (var book in books)
-    {
-        Console.WriteLine(book.Title + " - " + book.ISBN);
-    }
-}
+//void GetBook()
+//{
+//    using var context = new ApplicationDbContext();
+//    //var books = context.Books.Where(u=>u.ISBN.Contains("12"));
+//    //var books = context.Books.Where(u=>EF.Functions.Like(u.ISBN, "12%")).Max(u => u.Price);
+//    //var books = context.Books.Where(u=>EF.Functions.Like(u.ISBN, "12%")).Count();
+//    //var books = context.Books.Where(u => EF.Functions.Like(u.ISBN, "12%"));
+//    //var books = context.Books.Where(u => u.Price > 10).OrderBy(u => u.Title).ThenByDescending(u => u.ISBN);
 
-void AddBook()
-{
-    Book book = new Book()
-    {
-        Title = "Test",
-        ISBN = "Test",
-        Price = 10.23m,
-        Publisher_Id = 1
-    };
+//    //Pagination Methods
+//    var books = context.Books.Skip(0).Take(2);
 
-    using var context = new ApplicationDbContext();
-    var books = context.Books.Add(book);
-    context.SaveChanges();
-}
+//    //Console.WriteLine(book.Title + " - " + book.ISBN);
+//    foreach (var book in books)
+//    {
+//        Console.WriteLine(book.Title + " - " + book.ISBN);
+//    }
+//}
+////void GetBook()
+////{
+////    using var context = new ApplicationDbContext();
+////    Book book = context.Books.First();
+////    Console.WriteLine(book.Title + " - " + book.ISBN);
+////}
+
+////void GetBook()
+////{
+////    try
+////    {
+////    using var context = new ApplicationDbContext();
+////    var book = context.Fluent_Books.FirstOrDefault();
+////    Console.WriteLine(book.Title + " - " + book.ISBN);
+////    }catch(Exception e)
+////    {
+
+////    }
+////}
+
+//void GetAllBooks()
+//{
+//    using var context = new ApplicationDbContext();
+//    var books = context.Books.ToList();
+//    foreach (var book in books)
+//    {
+//        Console.WriteLine(book.Title + " - " + book.ISBN);
+//    }
+//}
+
+//void AddBook()
+//{
+//    Book book = new Book()
+//    {
+//        Title = "Test",
+//        ISBN = "Test",
+//        Price = 10.23m,
+//        Publisher_Id = 1
+//    };
+
+//    using var context = new ApplicationDbContext();
+//    var books = context.Books.Add(book);
+//    context.SaveChanges();
+//}
