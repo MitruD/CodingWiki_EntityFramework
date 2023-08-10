8585fb1c4ceb72b130dc88ae13197b7d2fd52369
Cronos <dimamitru@gmail.com>
2023-08-10 10:00:24 +0300

    Publisher and Author CRUD

--- CodingWiki_Web/Controllers/PublisherController.cs
+++ CodingWiki_Web/Controllers/PublisherController.cs
@@ -13,16 +13,38 @@ namespace CodingWiki_Web.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            List<Publisher> objList = _db.Publishers.ToList();
+            return View(objList);
         }
 
         public IActionResult Upsert(int? id)
         {
-            return View();
+            Publisher obj = new();
+            if (id == null || id == 0)
+            {
+                //Create
+                return View(obj);
+            }
+            //Edit
+            obj = _db.Publishers.FirstOrDefault(u => u.Publisher_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
         }
 
         public async Task<IActionResult> Delete(int id)
         {
+            Publisher obj = new();
+            obj = _db.Publishers.FirstOrDefault(u => u.Publisher_Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Publishers.Remove(obj);
+            await _db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
@@ -30,7 +52,22 @@ namespace CodingWiki_Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Upsert(Publisher obj)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                if (obj.Publisher_Id == 0)
+                {
+                    //create
+                    await _db.Publishers.AddAsync(obj);
+                }
+                else
+                {
+                    //update
+                    _db.Publishers.Update(obj);
+                }
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
         }
     }
 }
