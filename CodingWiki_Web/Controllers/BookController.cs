e26990ca132763872172e8a55a8a05b055324f6e
Cronos <dimamitru@gmail.com>
2023-08-12 13:27:56 +0300

    Explicit Loading in EF Core

--- CodingWiki_Web/Controllers/BookController.cs
+++ CodingWiki_Web/Controllers/BookController.cs
@@ -16,7 +16,16 @@ namespace CodingWiki_Web.Controllers
         public IActionResult Index()
         {
             List<Book> objList = _db.Books.ToList();
+            foreach(var obj in objList)
+            {
+                //Least effeicient
+                //obj.Publisher = _db.Publishers.Find(obj.Publisher_Id);
+
+                //More  effeicient
+                _db.Entry(obj).Reference(u=>u.Publisher).Load();
+            }
             return View(objList);
+
         }
 
         public IActionResult Upsert(int? id)
